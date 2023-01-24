adaa3afec899cbb252ff2258550af2f92bcea1ce
PeterfiLaci <113706063+placi0325@users.noreply.github.com>
2023-01-24 14:44:43 +0100

    update css

--- react-and-morty/src/components/Home.js
+++ react-and-morty/src/components/Home.js
@@ -3,6 +3,7 @@ import logo from '../img/logo.png'
 const Home = (props) => {
  return (<div className='header'>
     <img className='logo' src={logo} alt='logo'></img>
+    <p>On our page you can browse all of the characters and locations of Rick and Morty!</p>
     <div className='button-container'>
         <button className='button'>Characters</button>
         <button className='button'>Locations</button>
