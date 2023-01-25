3516434d7a086e438e8137c67fbc146203acab3c
Toth-Matrai Boroka <113712881+tmboroka@users.noreply.github.com>
2023-01-25 14:37:58 +0100

    location css

--- react-and-morty/src/components/LocationListItem.js
+++ react-and-morty/src/components/LocationListItem.js
@@ -1,13 +1,39 @@
+import planet from "../img/planet.jpg"
+import cluster from '../img/cluster.jpg'
+import dream from '../img/dream.jpg'
+import menagerie from '../img/menagerie.jpg'
+import resort from '../img/resort.webp'
+import tv from '../img/tv.jpg'
+import spacestation from '../img/spacestation.webp'
+
 const LocationListItem = (props) => {
     let img;
     switch (props.location.type) {
-            case "Planet":
-                img = "../img/planet.jpg"
-                break;
-
-            default:
-                break;
-        }
+        case "Planet":
+            img = planet;
+            break;
+        case "Cluster":
+            img = cluster;
+            break;
+        case "Dream":
+            img = dream;
+            break;
+        case "Space station":
+            img = spacestation;
+            break;
+        case "Menagerie":
+            img = menagerie;
+            break;
+        case "TV":
+            img = tv;
+            break;
+        case "Resort":
+            img = resort;
+            break;
+        default:
+            img=planet
+            break;
+    }
     const content = <div className='locationListItem'>
         <img className="locationImage" src={img} alt={props.location.name}></img>
         <div className='locationName'>{props.location.name}</div>
