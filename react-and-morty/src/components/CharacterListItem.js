17a44d89811e67cd2d5bb65032e1a8c6ad9bf5d7
Le Marcell <73469368+felhos@users.noreply.github.com>
2023-01-25 11:34:41 +0100

    prettify character list

--- react-and-morty/src/components/CharacterListItem.js
+++ react-and-morty/src/components/CharacterListItem.js
@@ -1,6 +1,6 @@
 const CharacterListItem = (props) => {
     const content = <div className='characterListItem'>
-        <img className="characterImage" src={props.character.image}></img>
+        <img className="characterImage" src={props.character.image} alt={props.character.name}></img>
         <div className='characterName'>{props.character.name}</div>
     </div>;
 
