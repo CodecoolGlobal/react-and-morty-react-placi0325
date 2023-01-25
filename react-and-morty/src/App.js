af03ebd515ef6ffb06f3b5cd44d36ef1ed780614
Le Marcell <73469368+felhos@users.noreply.github.com>
2023-01-25 10:40:15 +0100

    create character list

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -3,24 +3,15 @@ import "./App.css";
 import { useCharacters, useLocations } from "./api/useData";
 import Home from "./components/Home";
 import Header from "./components/Header";
+import CharacterList from "./components/CharacterList";
 
 function App () {
-  const characters = useCharacters(1);
-  const locations = useLocations(1);
-  console.log(locations)
-  
+
 
   return (<div className="container">
     <Header/>
     <Home />
-    {/*<div>
-      {characters && characters.results.map((char)=>
-            (<div>{char.name}</div>))}
-    </div>}
-    {<div>
-      {locations && locations.results.map((char)=>
-            (<div>{char.name}</div>))}
-      </div>*/}
+    <CharacterList />
   </div>)
 }         
 
